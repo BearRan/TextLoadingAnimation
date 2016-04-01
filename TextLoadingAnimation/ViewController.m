fa1543c09cdf98daa1a33edc9ab666e578e8cb2e
Bear <648070256@qq.com>
2016-04-01 17:33:59 +0800

    动画基本完成

--- TextLoadingAnimation/ViewController.m
+++ TextLoadingAnimation/ViewController.m
@@ -10,6 +10,9 @@
 #import "TextLoadingView.h"
 
 @interface ViewController ()
+{
+    TextLoadingView *textLoagdingView;
+}
 
 @end
 
@@ -18,20 +21,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    CGFloat loadingView_width = 100;
+    CGFloat loadingView_width = 300;
     
-    TextLoadingView *textLoagdingView = [[TextLoadingView alloc] initWithFrame:CGRectMake(0, 0, loadingView_width, loadingView_width)];
+    textLoagdingView = [[TextLoadingView alloc] initWithFrame:CGRectMake(0, 0, loadingView_width, loadingView_width)];
     [self.view addSubview:textLoagdingView];
     [textLoagdingView BearSetCenterToParentViewWithAxis:kAXIS_X_Y];
     
-    
+    UIButton *startBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 150, 50)];
+    startBtn.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.6];
+    [startBtn setTitle:@"start" forState:UIControlStateNormal];
+    [startBtn addTarget:self action:@selector(startBtnEvent) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:startBtn];
+    [startBtn BearSetRelativeLayoutWithDirection:kDIR_DOWN destinationView:nil parentRelation:YES distance:50 center:YES];
+}
+
+- (void)startBtnEvent
+{
     [textLoagdingView textLineAnimation_Start];
     
-    
-    CGFloat delayTime = 4.0f;
+    CGFloat delayTime = 5.0f;
     dispatch_time_t timer = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayTime *NSEC_PER_SEC));
     dispatch_after(timer, dispatch_get_main_queue(), ^{
-    [textLoagdingView textLineAnimation_End];
+        [textLoagdingView textLineAnimation_End];
     });
 }
 
